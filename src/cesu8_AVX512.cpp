0c05ae0ba2ccc6ccd73c472abe4b7e79719dce6f
Marco Schroeder <marco.schroeder@protonmail.com>
2023-08-17 08:25:07 +0200

    Partial refactoring

--- src/cesu8_AVX512.cpp
+++ src/cesu8_AVX512.cpp
@@ -3,110 +3,145 @@
 
 #include <immintrin.h>
 
-#include <cstdint>
 #include <cassert>
+#include <cstdint>
 
 using namespace cesu8::impl;
 
 namespace cesu8 {
 
-__m512i x_mm512_alignrvar_epi32(const __m512i hi, const __m512i lo, int shift) {
-    switch (shift) {
-        case 0: return lo; break;
-        case 1: return _mm512_alignr_epi32(hi, lo, 1); break;
-        case 2: return _mm512_alignr_epi32(hi, lo, 2); break;
-        case 3: return _mm512_alignr_epi32(hi, lo, 3); break;
-        case 4: return _mm512_alignr_epi32(hi, lo, 4); break;
-        case 5: return _mm512_alignr_epi32(hi, lo, 5); break;
-        case 6: return _mm512_alignr_epi32(hi, lo, 6); break;
-        case 7: return _mm512_alignr_epi32(hi, lo, 7); break;
-        case 8: return _mm512_alignr_epi32(hi, lo, 8); break;
-        case 9: return _mm512_alignr_epi32(hi, lo, 9); break;
-        case 10: return _mm512_alignr_epi32(hi, lo, 10); break;
-        case 11: return _mm512_alignr_epi32(hi, lo, 11); break;
-        case 12: return _mm512_alignr_epi32(hi, lo, 12); break;
-        case 13: return _mm512_alignr_epi32(hi, lo, 13); break;
-        case 14: return _mm512_alignr_epi32(hi, lo, 14); break;
-        case 15: return _mm512_alignr_epi32(hi, lo, 15); break;
-        case 16: return hi; break;
-        default:
-            assert(false);
-    }
+__m512i x_mm512_alignrvar_epi32(const __m512i hi, const __m512i lo, int shift)
+{
+        switch (shift) {
+                case 0:
+                        return lo;
+                        break;
+                case 1:
+                        return _mm512_alignr_epi32(hi, lo, 1);
+                        break;
+                case 2:
+                        return _mm512_alignr_epi32(hi, lo, 2);
+                        break;
+                case 3:
+                        return _mm512_alignr_epi32(hi, lo, 3);
+                        break;
+                case 4:
+                        return _mm512_alignr_epi32(hi, lo, 4);
+                        break;
+                case 5:
+                        return _mm512_alignr_epi32(hi, lo, 5);
+                        break;
+                case 6:
+                        return _mm512_alignr_epi32(hi, lo, 6);
+                        break;
+                case 7:
+                        return _mm512_alignr_epi32(hi, lo, 7);
+                        break;
+                case 8:
+                        return _mm512_alignr_epi32(hi, lo, 8);
+                        break;
+                case 9:
+                        return _mm512_alignr_epi32(hi, lo, 9);
+                        break;
+                case 10:
+                        return _mm512_alignr_epi32(hi, lo, 10);
+                        break;
+                case 11:
+                        return _mm512_alignr_epi32(hi, lo, 11);
+                        break;
+                case 12:
+                        return _mm512_alignr_epi32(hi, lo, 12);
+                        break;
+                case 13:
+                        return _mm512_alignr_epi32(hi, lo, 13);
+                        break;
+                case 14:
+                        return _mm512_alignr_epi32(hi, lo, 14);
+                        break;
+                case 15:
+                        return _mm512_alignr_epi32(hi, lo, 15);
+                        break;
+                case 16:
+                        return hi;
+                        break;
+                default:
+                        assert(false);
+        }
 }
 
+__m512i x_mm512_alignr_epi8(const __m512i hi, const __m512i lo, unsigned shift)
+{
+        assert(shift < 64);
 
-__m512i x_mm512_alignr_epi8(const __m512i hi, const __m512i lo, unsigned shift) {
+        const int s32 = shift / 4; // shift amount in 32-bit words
+        const int s8  = shift % 4; // shift amount within 32-bit word
 
-    assert(shift < 64);
+        if (s8 == 0) {
+                return x_mm512_alignrvar_epi32(hi, lo, s32);
+        }
+        else {
+                const __m512i L = x_mm512_alignrvar_epi32(hi, lo, s32);
+                const __m512i H = x_mm512_alignrvar_epi32(hi, lo, s32 + 1);
 
-    const int s32 = shift / 4; // shift amount in 32-bit words
-    const int s8  = shift % 4; // shift amount within 32-bit word
-
-    if (s8 == 0) {
-        return x_mm512_alignrvar_epi32(hi, lo, s32);
-    } else {
-        const __m512i L = x_mm512_alignrvar_epi32(hi, lo, s32);
-        const __m512i H = x_mm512_alignrvar_epi32(hi, lo, s32 + 1);
-
-        return _mm512_or_si512(
-                _mm512_srli_epi32(L, s8 * 8),
-                _mm512_slli_epi32(H, (4 - s8) * 8)
-        );
-    }
+                return _mm512_or_si512(
+                    _mm512_srli_epi32(L, s8 * 8), _mm512_slli_epi32(H, (4 - s8) * 8));
+        }
 }
 
-static __m512i avx512_push_last_byte_of_a_to_b(__m512i a, __m512i b) {
-//   __m512i indexes = _mm512_set_epi8(0x3E, 0x3D, 0x3C, 0x3B, 0x3A, 0x39, 0x38, 0x37,
-//                                     0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, 0x2F,
-//                                     0x2E, 0x2D, 0x2C, 0x2B, 0x2A, 0x29, 0x28, 0x27,
-//                                     0x26, 0x25, 0x24, 0x23, 0x22, 0x21, 0x20, 0x1F,
-//                                     0x1E, 0x1D, 0x1C, 0x1B, 0x1A, 0x19, 0x18, 0x17,
-//                                     0x16, 0x15, 0x14, 0x13, 0x12, 0x11, 0x10, 0x0F,
-//                                     0x0E, 0x0D, 0x0C, 0x0B, 0x0A, 0x09, 0x08, 0x07,
-//                                     0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00, 0x7F);
+static __m512i avx512_push_last_byte_of_a_to_b(__m512i a, __m512i b)
+{
+        //   __m512i indexes = _mm512_set_epi8(0x3E, 0x3D, 0x3C, 0x3B, 0x3A, 0x39, 0x38, 0x37,
+        //                                     0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, 0x2F,
+        //                                     0x2E, 0x2D, 0x2C, 0x2B, 0x2A, 0x29, 0x28, 0x27,
+        //                                     0x26, 0x25, 0x24, 0x23, 0x22, 0x21, 0x20, 0x1F,
+        //                                     0x1E, 0x1D, 0x1C, 0x1B, 0x1A, 0x19, 0x18, 0x17,
+        //                                     0x16, 0x15, 0x14, 0x13, 0x12, 0x11, 0x10, 0x0F,
+        //                                     0x0E, 0x0D, 0x0C, 0x0B, 0x0A, 0x09, 0x08, 0x07,
+        //                                     0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00, 0x7F);
 
-//   return _mm512_permutex2var_epi8(b, indexes, a);
-  //return indexes;
-  return x_mm512_alignr_epi8(a, b, 63);
+        //   return _mm512_permutex2var_epi8(b, indexes, a);
+        //return indexes;
+        return x_mm512_alignr_epi8(a, b, 63);
 }
 
-static __m512i avx512_push_last_2bytes_of_a_to_b(__m512i a, __m512i b) {
-//   __m512i indexes = _mm512_set_epi64(0x3D3C3B3A39383736, 0x3534333231302F2E,
-//                                      0x2D2C2B2A29282726, 0x2524232221201F1E,
-//                                      0x1D1C1B1A19181716, 0x1514131211100F0E,
-//                                      0x0D0C0B0A09080706, 0x0504030201007F7E);
-//   //return _mm512_permutex2var_epi8(b, indexes, a);
-//   return indexes;
+static __m512i avx512_push_last_2bytes_of_a_to_b(__m512i a, __m512i b)
+{
+        //   __m512i indexes = _mm512_set_epi64(0x3D3C3B3A39383736, 0x3534333231302F2E,
+        //                                      0x2D2C2B2A29282726, 0x2524232221201F1E,
+        //                                      0x1D1C1B1A19181716, 0x1514131211100F0E,
+        //                                      0x0D0C0B0A09080706, 0x0504030201007F7E);
+        //   //return _mm512_permutex2var_epi8(b, indexes, a);
+        //   return indexes;
         return x_mm512_alignr_epi8(a, b, 62);
 }
 
-static __m512i avx512_push_last_3bytes_of_a_to_b(__m512i a, __m512i b) {
-//   __m512i indexes = _mm512_set_epi64(0x3C3B3A3938373635, 0x34333231302F2E2D,
-//                                      0x2C2B2A2928272625, 0x24232221201F1E1D,
-//                                      0x1C1B1A1918171615, 0x14131211100F0E0D,
-//                                      0x0C0B0A0908070605, 0x04030201007F7E7D);
-//   //return _mm512_permutex2var_epi8(b, indexes, a);
-//   return indexes;
+static __m512i avx512_push_last_3bytes_of_a_to_b(__m512i a, __m512i b)
+{
+        //   __m512i indexes = _mm512_set_epi64(0x3C3B3A3938373635, 0x34333231302F2E2D,
+        //                                      0x2C2B2A2928272625, 0x24232221201F1E1D,
+        //                                      0x1C1B1A1918171615, 0x14131211100F0E0D,
+        //                                      0x0C0B0A0908070605, 0x04030201007F7E7D);
+        //   //return _mm512_permutex2var_epi8(b, indexes, a);
+        //   return indexes;
         return x_mm512_alignr_epi8(a, b, 61);
-
 }
 
 bool is_valid_cesu8_AVX512(byte const* str, std::size_t len)
 {
-        static __m512i const lookup_table3 = _mm512_setr4_epi32(0x33333333, 0x33333333,
-                                                                0x3C3C3C3C, 0x00333333);
-        static __m512i const lookup_table2 = _mm512_setr4_epi32(0x14141414, 0x14141414,
-                                                                0x5A9A3A3A, 0x00111111);
-        static __m512i const lookup_table1_high = _mm512_setr4_epi32(0x05050505, 0x05050505,
-                                                                     0x0D0D0D0D, 0x00E70D1D);
-        static __m512i const lookup_table1_low = _mm512_setr4_epi32(0x0F0F1F3F, 0x0F0F0F0F,
-                                                                    0x0F0F0F0F, 0x0F0FCF0F);
+        static __m512i const lookup_table3 =
+            _mm512_setr4_epi32(0x33333333, 0x33333333, 0x3C3C3C3C, 0x00333333);
+        static __m512i const lookup_table2 =
+            _mm512_setr4_epi32(0x14141414, 0x14141414, 0x5A9A3A3A, 0x00111111);
+        static __m512i const lookup_table1_high =
+            _mm512_setr4_epi32(0x05050505, 0x05050505, 0x0D0D0D0D, 0x00E70D1D);
+        static __m512i const lookup_table1_low =
+            _mm512_setr4_epi32(0x0F0F1F3F, 0x0F0F0F0F, 0x0F0F0F0F, 0x0F0FCF0F);
 
         __m512i prev_bytes =
             _mm512_setzero_si512(); // Zero values will be interpeted as preceding ascii in first loop iteration
-        __m512i has_error            = _mm512_setzero_si512();
-        __m512i prev_high_surrogates = _mm512_setzero_si512();
-        __mmask64 has_error_header = 0;
+        __m512i   has_error            = _mm512_setzero_si512();
+        __m512i   prev_high_surrogates = _mm512_setzero_si512();
+        __mmask64 has_error_header     = 0;
         while (len >= 64) {
                 // Load next bytes
                 __m512i bytes3 = _mm512_loadu_si512(reinterpret_cast<__m512i const*>(str));
@@ -134,14 +169,16 @@ bool is_valid_cesu8_AVX512(byte const* str, std::size_t len)
                 __m512i error_flags_abd = _mm512_and_si512(temp, result3);
 
                 // Incomplete surrogates
-                __m512i high_surrogates         = _mm512_and_si512(temp, _mm512_set1_epi8(0x80));
-                __m512i low_surrogates          = _mm512_and_si512(temp, _mm512_set1_epi8(0x40));
-                low_surrogates                  = _mm512_slli_epi16(low_surrogates, 1);
-                __m512i shifted_high_surrogates = avx512_push_last_3bytes_of_a_to_b(high_surrogates, prev_high_surrogates);
+                __m512i high_surrogates = _mm512_and_si512(temp, _mm512_set1_epi8(0x80));
+                __m512i low_surrogates  = _mm512_and_si512(temp, _mm512_set1_epi8(0x40));
+                low_surrogates          = _mm512_slli_epi16(low_surrogates, 1);
+                __m512i shifted_high_surrogates =
+                    avx512_push_last_3bytes_of_a_to_b(high_surrogates, prev_high_surrogates);
                 __m512i error_flags_e = _mm512_xor_si512(shifted_high_surrogates, low_surrogates);
 
                 // Illegal header
-                has_error_header = _kor_mask64(has_error_header, _mm512_cmpeq_epi8_mask(_mm512_set1_epi8(0xF), nibbles3));
+                has_error_header = _kor_mask64(
+                    has_error_header, _mm512_cmpeq_epi8_mask(_mm512_set1_epi8(0xF), nibbles3));
 
                 // Store error conditions
                 has_error = _mm512_or_si512(has_error, error_flags_abd);
@@ -168,13 +205,13 @@ bool is_valid_cesu8_AVX512(byte const* str, std::size_t len)
 
         // Value error must not be considered only structural as we start revalidation anyways
         size_t offset = 0;
-        if (is_ed_a_c0(str[-5], str[-4], str[-3])) {
+        if (is_ed_a_cb(str[-5], str[-4], str[-3])) {
                 offset = 5;
         }
-        else if (is_ed_a_c0(str[-4], str[-3], str[-2])) {
+        else if (is_ed_a_cb(str[-4], str[-3], str[-2])) {
                 offset = 4;
         }
-        else if (is_ed_a_c0(str[-3], str[-2], str[-1])) {
+        else if (is_ed_a_cb(str[-3], str[-2], str[-1])) {
                 offset = 3;
         }
         else if (is_c3(str[-2])) { // Checks surrogate pair as well
--- src/cesu8_SSE.cpp
+++ src/cesu8_SSE.cpp
@@ -9,96 +9,133 @@ using namespace cesu8::impl;
 
 namespace cesu8 {
 
+static __m128i extract_high_nibbles(__m128i a)
+{
+    __m128i low_nibble_mask = _mm_set1_epi8(0x0F);
+    __m128i a_shifted       = _mm_srli_epi16(a, 4);
+    __m128i high_nibbles    = _mm_and_si128(a_shifted, low_nibble_mask);
+    return high_nibbles;
+}
+
+static __m128i extract_low_nibbles(__m128i a)
+{
+    __m128i low_nibble_mask = _mm_set1_epi8(0x0F);
+    __m128i low_nibbles     = _mm_and_si128(a, low_nibble_mask);
+    return low_nibbles;
+}
+
 bool is_valid_cesu8_SSE(byte const* str, std::size_t len)
 {
-        static __m128i const table3 = _mm_setr_epi8(CESU8_IMPLEMENTATION_LOOKUPTABLE_3_VALUES);
-        static __m128i const table2 = _mm_setr_epi8(CESU8_IMPLEMENTATION_LOOKUPTABLE_2_VALUES);
-        static __m128i const table1_high =
-            _mm_setr_epi8(CESU8_IMPLEMENTATION_LOOKUPTABLE_1HIGH_VALUES);
-        static __m128i const table1_low =
-            _mm_setr_epi8(CESU8_IMPLEMENTATION_LOOKUPTABLE_1LOW_VALUES);
+    // clang-format off
+    static __m128i const table3 = _mm_setr_epi8(
+        0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, // 0|xxx'xxxx (C1)
+        0x3C, 0x3C, 0x3C, 0x3C,                         // 10|xx'xxxx (CB)
+        0x33, 0x33,                                     // 110|x'xxxx (C2)
+        0x33,                                           // 1110|'xxxx (C3)
+        0x0                                             // 1111'xxxx (Illegal header)
+    );
+    static __m128i const table2 = _mm_setr_epi8(
+        0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14, 0x14, // 0|xxx'xxxx (C1)
+        0x3A, 0x3A, 0x9A, 0x5A,                         // 10|xx'xxxx (CB)
+        0x11, 0x11,                                     // 110|x'xxxx (C2)
+        0x11,                                           // 1110|'xxxx (C3)
+        0x0                                             // 1111'xxxx (Illegal header)
+    );
+    static __m128i const table1_hi = _mm_setr_epi8(
+        0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5,
+        0xD, 0xD, 0xD, 0xD,
+        0x1D, 0xD,
+        0xE7,
+        0x0
+    );
+    static __m128i const table1_lo = _mm_setr_epi8(
+        0x3F, 0x1F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F,
+        0x0F, 0x0F, 0x0F, 0x0F,
+        0x0F, 0xCF,
+        0x0F,
+        0x0F
+    );
+    // clang-format on
 
-        __m128i prev_bytes =
-            _mm_setzero_si128(); // Zero values will be interpeted as preceding ascii in first loop iteration
-        __m128i has_error            = _mm_setzero_si128();
-        __m128i prev_high_surrogates = _mm_setzero_si128();
-        while (len >= 16) {
-                // Load next bytes
-                __m128i bytes3 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(str));
-                __m128i bytes2 = _mm_alignr_epi8(bytes3, prev_bytes, 15);
-                __m128i bytes1 = _mm_alignr_epi8(bytes3, prev_bytes, 14);
+    __m128i prev_bytes         = _mm_setzero_si128();
+    __m128i has_error          = _mm_setzero_si128();
+    __m128i prev_hi_surrogates = _mm_setzero_si128();
+    while (len >= 16) {
+        // Load next bytes
+        __m128i bytes3 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(str));
+        __m128i bytes2 = _mm_alignr_epi8(bytes3, prev_bytes, 15);
+        __m128i bytes1 = _mm_alignr_epi8(bytes3, prev_bytes, 14);
 
-                // Extract header bits by shifting entire register right by 4-bit and masking lowest 4 bits in each lane
-                __m128i nibbles3 = _mm_and_si128(_mm_srli_epi16(bytes3, 4), _mm_set1_epi8(0x0F));
-                __m128i nibbles2 = _mm_and_si128(_mm_srli_epi16(bytes2, 4), _mm_set1_epi8(0x0F));
-                __m128i nibbles1_low = _mm_and_si128(bytes1, _mm_set1_epi8(0x0F));
-                __m128i nibbles1_high =
-                    _mm_and_si128(_mm_srli_epi16(bytes1, 4), _mm_set1_epi8(0x0F));
+        // Extract header bits
+        __m128i nibbles3    = extract_high_nibbles(bytes3);
+        __m128i nibbles2    = extract_high_nibbles(bytes2);
+        __m128i nibbles1_lo = extract_low_nibbles(bytes1);
+        __m128i nibbles1_hi = extract_high_nibbles(bytes1);
 
-                // Table lookup
-                __m128i result3      = _mm_shuffle_epi8(table3, nibbles3);
-                __m128i result2      = _mm_shuffle_epi8(table2, nibbles2);
-                __m128i result1_low  = _mm_shuffle_epi8(table1_low, nibbles1_low);
-                __m128i result1_high = _mm_shuffle_epi8(table1_high, nibbles1_high);
-                __m128i temp = _mm_and_si128(result2, _mm_and_si128(result1_high, result1_low));
+        // Table lookup
+        __m128i result3    = _mm_shuffle_epi8(table3, nibbles3);
+        __m128i result2    = _mm_shuffle_epi8(table2, nibbles2);
+        __m128i result1_lo = _mm_shuffle_epi8(table1_lo, nibbles1_lo);
+        __m128i result1_hi = _mm_shuffle_epi8(table1_hi, nibbles1_hi);
 
-                // Detect error conditions A) Too short, B) Too long, D) Overlong
-                __m128i error_flags_abd = _mm_and_si128(temp, result3);
+        // Detect error conditions A) Too short, B) Too long, D) Overlong
+        __m128i temp = _mm_and_si128(result2, _mm_and_si128(result1_hi, result1_lo));
+        __m128i errors_abd = _mm_and_si128(temp, result3);
 
-                // Detect error condition E) Invalid surrogate
-                __m128i high_surrogates = _mm_and_si128(temp, _mm_set1_epi8(0x80));
-                __m128i low_surrogates  = _mm_and_si128(temp, _mm_set1_epi8(0x40));
-                low_surrogates          = _mm_slli_epi16(low_surrogates, 1);
-                __m128i shifted_high_surrogates =
-                    _mm_alignr_epi8(high_surrogates, prev_high_surrogates, 13);
-                __m128i error_flags_e = _mm_xor_si128(shifted_high_surrogates, low_surrogates);
+        // Detect error condition E) Incomplete surrogate
+        __m128i hi_surrogates = _mm_and_si128(temp, _mm_set1_epi8(0x80));
+        __m128i lo_surrogates = _mm_and_si128(temp, _mm_set1_epi8(0x40));
+        lo_surrogates         = _mm_slli_epi16(lo_surrogates, 1);
+        __m128i shifted_hi_surrogates =
+            _mm_alignr_epi8(hi_surrogates, prev_hi_surrogates, 13);
+        __m128i errors_e = _mm_xor_si128(shifted_hi_surrogates, lo_surrogates);
 
-                // Detect error condition C) Illegal header
-                __m128i error_flags_c = _mm_cmpeq_epi8(_mm_set1_epi8(0xF), nibbles3);
+        // Detect error condition C) Illegal header
+        __m128i errors_c = _mm_cmpeq_epi8(_mm_set1_epi8(0xF), nibbles3);
 
-                // Store error conditions
-                has_error = _mm_or_si128(has_error, error_flags_abd);
-                has_error = _mm_or_si128(has_error, error_flags_c);
-                has_error = _mm_or_si128(has_error, error_flags_e);
+        // Store error conditions
+        has_error = _mm_or_si128(has_error, errors_abd);
+        has_error = _mm_or_si128(has_error, errors_c);
+        has_error = _mm_or_si128(has_error, errors_e);
 
-                // Setup next iteration
-                prev_high_surrogates = high_surrogates;
-                prev_bytes           = bytes3;
-                str += 16;
-                len -= 16;
-        }
+        // Setup next iteration
+        prev_hi_surrogates = hi_surrogates;
+        prev_bytes         = bytes3;
+        str += 16;
+        len -= 16;
+    }
 
-        if (!_mm_testz_si128(has_error, has_error)) {
-                return false;
-        }
+    if (!_mm_testz_si128(has_error, has_error)) {
+        return false;
+    }
 
-        /*
+    /*
     The SSE validation iterates the string with a step size of 16. Remaining bytes have to be validated using
     the non-SIMD algorithm. The last bytes processed by the SSE validation may contain the start of a multi-byte
     code point which extends into the remaining bytes. The non-SIMD algorithm can not start validation in the middle
     of a code point, so str/len must potentially be adjusted to the last completly validated code point
     */
 
-        // Value error must not be considered only structural as we start revalidation anyways
-        uint32_t high_surrogates = _mm_movemask_epi8(prev_high_surrogates);
-        high_surrogates &= 0xE000; // extract 3 msbs
-        size_t offset = 0;
-        if (high_surrogates == (1u << 13)) {
-                offset = 5;
-        }
-        else if (high_surrogates == (1u << 14)) {
-                offset = 4;
-        }
-        else if (high_surrogates == (1u << 15)) {
-                offset = 3;
-        }
-        else if (is_c3(str[-2])) { // Checks surrogate pair as well
-                offset = 2;
-        }
-        else if ((str[-1] & 0xC0) == 0xC0) { // Checks 2 and 3 byte header
-                offset = 1;
-        }
-        return is_valid_cesu8_scalar(str - offset, len + offset);
+    // Value error must not be considered only structural as we start revalidation anyways
+    uint32_t hi_surrogates = _mm_movemask_epi8(prev_hi_surrogates);
+    hi_surrogates &= 0xE000; // extract 3 msbs
+    size_t offset = 0;
+    if (hi_surrogates == (1u << 13)) {
+        offset = 5;
+    }
+    else if (hi_surrogates == (1u << 14)) {
+        offset = 4;
+    }
+    else if (hi_surrogates == (1u << 15)) {
+        offset = 3;
+    }
+    else if (is_c3(str[-2])) { // Checks surrogate pair as well
+        offset = 2;
+    }
+    else if ((str[-1] & 0xC0) == 0xC0) { // Checks 2 and 3 byte header
+        offset = 1;
+    }
+    return is_valid_cesu8_scalar(str - offset, len + offset);
 }
 
 } // namespace cesu8
